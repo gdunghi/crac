9a028b00eb72ef50819180fc737fba0e61ae28df
Jirayu Photisan <gdunghi@gmail.com>
2025-03-27 16:31:53 +0700

    fix

--- src/main/kotlin/org/example/ui/crac/Controller.kt
+++ src/main/kotlin/org/example/ui/crac/Controller.kt
@@ -1,13 +1,16 @@
 package org.example.ui.crac
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Controller {
+class Controller(val environment: Environment) {
 
     @GetMapping("/test")
     fun test(): String {
-        return "OK"
+        val mysqlUri = environment.getProperty("spring.datasource.url")
+        println("Connected to MySQL: $mysqlUri")
+        return mysqlUri!!
     }
 }
